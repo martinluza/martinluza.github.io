6bdf25d286b3228f677c14ac7f5ffc63fdd3cc2a
martinluza <martin.luzardo1@gmail.com>
2022-08-21 01:35:29 -0300

    entrega1

---
+++ js/login.js
@@ -0,0 +1,22 @@
+let boton = document.getElementById('ingBtn');
+
+boton.addEventListener('click', function(evento){
+    
+    let campos = document.getElementsByClassName('form-control');
+    todoOk = true;
+
+    for (let i = 0; i < campos.length; i++){
+        if (campos[i].value.length === 0){
+            todoOk = false;
+        }
+    }
+
+    if (todoOk) {
+        window.location.replace("index2.html");
+    }
+    else {
+        alert(error);
+    }
+
+})
+
---
+++ js/products.js
@@ -0,0 +1,39 @@
+let currentProductsArray = [];
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.products.length; i++){
+        let product = currentProductsArray.products[i];
+
+        htmlContentToAppend += `
+        <div onclick="setProductID(${product.id})" class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
+                        <small class="text-muted">${product.soldCount} vendidos</small>
+                    </div>
+                    <p class="mb-1">${product.description}</p>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+   
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductsArray = resultObj.data
+            showProductsList()
+            document.getElementById("cat-p").innerHTML += `<p class="lead">Verás aquí todos los productos de la categoría ${currentProductsArray.catName}</p>`;
+        }
+    });
+});
